62d1a406e85f391daa0af22ef75fc5059b740c07
Logan Hasson <logan.hasson@gmail.com>
2014-03-16 12:02:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoustonTest::Application.config.secret_key_base = 'a007e6b329cdb3d52f02b88934ddf6a78d26ba8f7d0ebbb7bbb76ed6d1a6371d83ce4dd8a8f94e90752876fb863637b1111b05288bfdcfe7238bf786559e19aa'
