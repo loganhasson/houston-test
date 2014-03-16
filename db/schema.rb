a701aac11502ac234ad85ef0be89ddfe3f57cff0
Logan Hasson <logan.hasson@gmail.com>
2014-03-16 12:11:23 -0400

    Set up Api controllers and first migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316160052) do
+ActiveRecord::Schema.define(version: 20140316161104) do
+
+  create_table "applications", force: true do |t|
+    t.string   "name"
+    t.string   "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "devices", force: true do |t|
     t.string   "token"
